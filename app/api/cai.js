d033bdb06292b40b98c1157ebf626b68e6982c07
FrierenDv <hackerxct007@gmail.com>
2024-01-22 15:24:02 +0700

    show log when authencated to CAI

--- app/api/cai.js
+++ app/api/cai.js
@@ -2,5 +2,7 @@ import { Chat } from "@xct007/chrt-ai";
 import config from "./config.js";
 
 const chat = new Chat();
-chat.authenticate(config.access_token, config.id_token);
+chat.authenticate(config.access_token, config.id_token).then(() => {
+	console.log("Authenticated");
+});
 export default chat;
